929b3c9038b7c3ca27508474059c6ec007a20d92
dima <dima.xipkov@gmail.com>
2023-08-27 03:05:39 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+// $(function name(params) {
+//     const Recommendations = new Swiper('.Recommendations', {
+//         // slidesPerView: 1,
+//         // loop: true,
+//         // loopedslides: 1,
+//         // slidesPerView: 2,
+//         // grid: {
+//         //     rows: 2,
+//         // },
+//         navigation: {
+//             nextEl: ".swiper-button-next",
+//             prevEl: ".swiper-button-prev",
+//         },
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.faq__accordion > li  >.faq__text').hide();
+
+//     $('.faq__accordion > li ').click(function () {
+//         if ($(this).hasClass("active")) {
+//             $(this).removeClass("active").find(".faq__text").slideUp();
+//         } else {
+//             $(".faq__accordion > li  .active .faq__text").slideUp();
+//             $(".faq__accordion > li  .active").removeClass("active");
+//             $(this).addClass("active").find(".faq__text").slideDown();
+//         }
+//         return false;
+//     });
+// });
+
+// --------------------
+/*	$(".menu a").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
+    });
+
+    /*для закрыть меню*/
+/*
+          $('.menu__btn').on('click', function () {
+              $('.menu__items').toggleClass('menu__items--active');
+          });
+          $('.menu__btn').on('click', function () {
+              $('.menu__btn').toggleClass('active');
+          });
+       
+        $('.home__slider').slick({
+            dots: true,
+            arrows: false,
+             responsive: [
+
+
+                 {
+                     breakpoint: 1150,
+                     settings: {
+                         slidesToShow: 4,
+                     }
+                 },
+                 {
+                     breakpoint: 1000,
+                     settings: {
+                         slidesToShow: 3,
+                     }
+                 },
+                 {
+                     breakpoint: 620,
+                     settings: {
+                         slidesToShow: 2,
+                         slidesToScroll: 2,
+                     }
+                 },
+             ]
+        });
+       
+       
+       */
+
